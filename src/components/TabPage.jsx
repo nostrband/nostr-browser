6c4a7fc99fafacd34a8408dc9b880783f34a67b2
Katherina Kaliaha <mellinda077@mail.ru>
2023-04-06 18:39:46 +0300

    feat: add libraru for render tabs

--- src/components/TabPage.jsx
+++ src/components/TabPage.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { Modal } from './Modal';
 import GetForm from './Form';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 export const TabPage = () => {
   const [tabs, setTabs] = useState([]);
@@ -13,18 +15,28 @@ export const TabPage = () => {
     setTabs([...tabs, ...data]);
   };
 
+  const [tabIndex, setTabIndex] = useState(0);
+
   return (
-    <div>
-      <button onClick={openModal}> Press me</button>
-      {tabs.length > 0 &&
-        tabs.map((tab, ind) => {
-          return (
-            <div key={tab.name + ind}>
-              <p>name: {tab.name}</p>
-              <p>text: {tab.text}</p>
-            </div>
-          );
-        })}
+    <div style={{ marginTop: 20 }}>
+      <button onClick={openModal} style={{ marginBottom: 20 }}>
+        {' '}
+        Press me
+      </button>
+      {tabs.length > 0 && (
+        <Tabs selectedIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
+          <TabList>
+            {tabs.length > 0 &&
+              tabs.map((tab, ind) => (
+                <Tab key={tab.name + ind}>{tab.name}</Tab>
+              ))}
+          </TabList>
+          {tabs.length > 0 &&
+            tabs.map((tab, ind) => (
+              <TabPanel key={tab.name + ind}>{tab.text}</TabPanel>
+            ))}
+        </Tabs>
+      )}
       <Modal activeModal={isOpen} setActive={closeModal}>
         <GetForm setActive={closeModal} onSubmit={addTab} />
       </Modal>

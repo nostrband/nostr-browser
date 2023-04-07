3c7df433ab1b0428f990f17c7b041d455d35a512
Katherina Kaliaha <mellinda077@mail.ru>
2023-04-07 14:54:51 +0300

    fix: change structure TabPage component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { TabPage } from './components/TabPage';
+import { Modal } from './components/Modal';
+import GetForm from './components/Form';
 
 function App() {
+  const [tabs, setTabs] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openModal = () => setIsOpen(true);
+  const closeModal = () => setIsOpen(false);
+
+  const addTab = (data) => {
+    setTabs([...tabs, data]);
+  };
+
   return (
-    <div className="App">
-      <TabPage />
+    <div className="App" style={{ marginTop: 20 }}>
+      <button onClick={openModal} style={{ marginBottom: 20 }}>
+        Press me
+      </button>
+      <TabPage data={tabs} />
+      <Modal activeModal={isOpen} setActive={closeModal}>
+        <GetForm setActive={closeModal} onSubmit={addTab} />
+      </Modal>
     </div>
   );
 }
--- src/components/TabPage.jsx
+++ src/components/TabPage.jsx
@@ -1,45 +1,26 @@
 import { useState } from 'react';
-import { Modal } from './Modal';
-import GetForm from './Form';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 
-export const TabPage = () => {
-  const [tabs, setTabs] = useState([]);
-  const [isOpen, setIsOpen] = useState(false);
-
-  const openModal = () => setIsOpen(true);
-  const closeModal = () => setIsOpen(false);
-
-  const addTab = (data) => {
-    setTabs([...tabs, data]);
-  };
-
+export const TabPage = ({ data }) => {
   const [tabIndex, setTabIndex] = useState(0);
 
   return (
-    <div style={{ marginTop: 20 }}>
-      <button onClick={openModal} style={{ marginBottom: 20 }}>
-        {' '}
-        Press me
-      </button>
-      {tabs.length > 0 && (
+    <>
+      {data.length > 0 && (
         <Tabs selectedIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
           <TabList>
-            {tabs.length > 0 &&
-              tabs.map((tab, ind) => (
+            {data.length > 0 &&
+              data.map((tab, ind) => (
                 <Tab key={tab.name + ind}>{tab.name}</Tab>
               ))}
           </TabList>
-          {tabs.length > 0 &&
-            tabs.map((tab, ind) => (
+          {data.length > 0 &&
+            data.map((tab, ind) => (
               <TabPanel key={tab.name + ind}>{tab.text}</TabPanel>
             ))}
         </Tabs>
       )}
-      <Modal activeModal={isOpen} setActive={closeModal}>
-        <GetForm setActive={closeModal} onSubmit={addTab} />
-      </Modal>
-    </div>
+    </>
   );
 };

c81f6ae31142bb8cca0964d2cd9940c890d96790
KristinaZhur <krist-y_92@mail.ru>
2023-04-11 23:25:28 +0300

    feat:add images for buttons

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import './App.scss';
 import { TabPage } from './components/TabPage';
 import { Modal } from './components/Modal';
 import GetForm from './components/Form';
+import imgGlobal from './global.png';
+import imgRelay from './relay.png';
 
 function App() {
   const [tabs, setTabs] = useState([]);
@@ -17,9 +19,16 @@ function App() {
 
   return (
     <div className="App" style={{ marginTop: 20 }}>
+      
       <button className = 'main--button' onClick={openModal} style={{ marginBottom: 20 }}>
+        <img className = 'app--image' src = {imgGlobal} alt = '' />
         Press me
       </button>
+      <button className = 'main--button' style={{ marginBottom: 20 }}>
+        <img className = 'app--image' src = {imgRelay} alt = '' />
+        Relays
+      </button>
+
       <TabPage data={tabs} />
       <Modal activeModal={isOpen} setActive={closeModal}>
         <GetForm setActive={closeModal} onSubmit={addTab} />

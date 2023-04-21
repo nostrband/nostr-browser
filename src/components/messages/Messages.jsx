95a7e18bd778cce1210951a1d90f5bb27236fc97
denis1989 <denis1989@bk.ru>
2023-04-21 13:36:50 +0300

    implemented logic for viewing messages kind 7

--- src/components/messages/Messages.jsx
+++ src/components/messages/Messages.jsx
@@ -1,12 +1,14 @@
 import {MessageKind1} from "./MessageKind1.jsx";
 import {MessageKind0} from "./MessageKind0.jsx";
 import {MessageKindOther} from "./MessageKindOther.jsx";
+import {MessageKind7} from "./MessageKind7.jsx";
 
 export const Messages = ({message}) => {
 
     const messagesMap = {
         0: <MessageKind0 message={message}/>,
-        1: <MessageKind1 message={message}/>
+        1: <MessageKind1 message={message}/>,
+        7: <MessageKind7 message={message}/>
     }
 
     return (

fda3e26d88d3f26de9c5b4901ea82cdd38a502df
Katherina Kaliaha <mellinda077@mail.ru>
2023-05-10 22:39:54 +0300

    feat: add change view tabs

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -25,6 +25,9 @@ export const Tabs = ({
     event.stopPropagation();
     openFilterModal(index);
   };
+  const widthTab = () => {
+    return Math.floor((100 - 5) / tabs.length);
+  };
 
   return (
     <>
@@ -111,11 +114,9 @@ export const Tabs = ({
                     key={item.url + item.index}
                   >
                     <button
-                      onClick={openTab}
+                      // onClick={openTab}
                       data-index={item.index}
-                      className={`nav-link ${
-                        item.index === active ? 'active' : ''
-                      } tabPil`}
+                      className={`nav-link tabPil`}
                       id="profile-tab"
                       data-bs-toggle="tab"
                       data-bs-target="#pills-home"
@@ -129,9 +130,7 @@ export const Tabs = ({
                         onClick={(event) => openFilter(event, item.index)}
                       ></span>
                       {item.url} <br />
-                      {item.filter === null
-                        ? null
-                        : JSON.stringify(item.filter)}
+                      {item.filter === null ? null : item.filter}
                       <span
                         className="closeIcon"
                         aria-hidden="true"
@@ -143,16 +142,18 @@ export const Tabs = ({
                   </li>
                 ))}
               </ul>
-              <div className="tab-content" id="pills-tabContent">
+              <div
+                className="tab-content d-flex row-gap-10"
+                id="pills-tabContent"
+              >
                 {tabs.map((item) => (
                   <div
                     key={item.url + item.index}
-                    className={`tab-pane fade show ${
-                      item.index === active ? 'active' : ''
-                    }`}
+                    className={`tab-pane fade show active me-5`}
                     id="home"
                     role="tabpanel"
                     aria-labelledby="pills-home-tab"
+                    style={{ width: `${widthTab()}%` }}
                   >
                     {item.relay}
                   </div>

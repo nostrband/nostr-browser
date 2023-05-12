6039f2d691e9d26730fb15c0b0a2035c179220d9
Katherina Kaliaha <mellinda077@mail.ru>
2023-05-12 10:32:17 +0300

    fix: fix table style

--- src/components/messages/MessageFooter.jsx
+++ src/components/messages/MessageFooter.jsx
@@ -1,33 +1,53 @@
 import React from 'react';
-import Nostr from "../../Nostr.ts";
-import {NostrBandLink} from "../NostrBandLink.jsx";
+import Nostr from '../../Nostr.ts';
+import { NostrBandLink } from '../NostrBandLink.jsx';
 
-export const MessageFooter = ({tags}) => {
-    return (
-        <> {tags && tags.length > 0 && (
-            <div className='card-footer'>
-                <p>Tags:</p>
-                <table className="table">
-                    <thead>
-                    </thead>
-                    <tbody>
-                    {tags.map((tag, index) =>
-                        <tr key={index + 'tagUniqueIndex'}> {tag.map((item, index) => {
-                            if (index === 1 && tag[0] === 'p') {
-                                return <td key={index + 'itemUniqueIndex'}><NostrBandLink
-                                    postfix={Nostr.encodeAuthorPubKey(item)} value={item}/></td>
-                            }
-                            if (index === 1 && tag[0] === 'e') {
-                                return <td key={index + 'itemUniqueIndex'}><NostrBandLink
-                                    postfix={Nostr.encodeEventId(item)} value={item}/></td>
-                            }
-                            return <td key={index + 'itemUniqueIndex'}>{item+'dfd'}</td>
-                        })}</tr>
-                    )}
-                    </tbody>
-                </table>
-            </div>
-        )}
-        </>
-    );
+export const MessageFooter = ({ tags }) => {
+  return (
+    <>
+      {' '}
+      {tags && tags.length > 0 && (
+        <div className="card-footer">
+          <p>Tags:</p>
+          <table className="table">
+            <thead></thead>
+            <tbody>
+              {tags.map((tag, index) => (
+                <tr
+                  key={index + 'tagUniqueIndex'}
+                  style={{ wordBreak: 'break-word' }}
+                >
+                  {tag.map((item, index) => {
+                    if (index === 1 && tag[0] === 'p') {
+                      return (
+                        <td key={index + 'itemUniqueIndex'}>
+                          <NostrBandLink
+                            postfix={Nostr.encodeAuthorPubKey(item)}
+                            value={item}
+                          />
+                        </td>
+                      );
+                    }
+                    if (index === 1 && tag[0] === 'e') {
+                      return (
+                        <td key={index + 'itemUniqueIndex'}>
+                          <NostrBandLink
+                            postfix={Nostr.encodeEventId(item)}
+                            value={item}
+                          />
+                        </td>
+                      );
+                    }
+                    return (
+                      <td key={index + 'itemUniqueIndex'}>{item + 'dfd'}</td>
+                    );
+                  })}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
+    </>
+  );
 };

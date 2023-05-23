05be293c5e836439bbf5aa3d11c176e814a1042b
denis1989 <denis1989@bk.ru>
2023-05-23 18:04:33 +0300

    updated link to note

--- src/components/messages/MessageHeader.jsx
+++ src/components/messages/MessageHeader.jsx
@@ -7,19 +7,17 @@ import { getAuthorData } from '../../utils/helpers';
 
 export const MessageHeader = ({ message, showProfiles }) => {
   const formatDate = (createdAt) => {
-    return moment(createdAt * 1000).format('YYYY-MM-DD HH:mm:SS'); 
-    
+    return moment(createdAt * 1000).format('YYYY-MM-DD HH:mm:SS');
   };
-  console.log (message.pubkey)
   const { name, picture } = getAuthorData(message);
 
   return ( 
     <>
       {message && (
         <p className="card-header">
-          Kind: {message.kind} CreatedAt: {formatDate(message.created_at)} (
-          {message.created_at}) ID: <a href = {'https://nostr.band/' + message.pubkey } target = 'blank'>{message.pubkey}</a> <br />
-          Author:
+          Kind: {message.kind} CreatedAt: {formatDate(message.created_at)} ({message.created_at}) <br />
+          ID: <NostrBandLink postfix={Nostr.encodeEventId(message.id)} value={message.id}/>  <br />
+          Author:&nbsp;
           {showProfiles ? (
             <>
               <img src={picture} alt="" width={30} className="me-2 ms-2" />
